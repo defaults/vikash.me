e25423a772baa59c4c3622d097ffc13ae53a3183
Vikash Kumar <mailkumarvikash@gmail.com>
2014-07-15 03:01:38 +0530

    removed error

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
 import webapp2
 import jinja2
+import os
+import datetime
+import cgi
+import json
 
-JINJA_ENVIRONMENT = jinja2.Environment(
-    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
-    autoescape=True,
-    extensions=['jinja2.ext.autoescape'])
 
 class MainHandler(webapp2.RequestHandler):
-
-    template = JINJA_ENVIRONMENT.get_template('home.html')
-    self.response.write(template.render())
-
+    self.response.render('index.html')
 
 app = webapp2.WSGIApplication(
     [
