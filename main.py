a982229081d9c1e7f3531c96f385c28d288c068c
Vikash Kumar <mailkumarvikash@gmail.com>
2014-11-03 23:17:25 +0530

    added picture

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ class BaseHandler(webapp2.RequestHandler):
 
     @webapp2.cached_property
     def jinja2(self):
+        gfiohfjg
         # Returns a Jinja2 renderer cached in the app registry.
         return jinja2.get_jinja2(app=self.app)
 
