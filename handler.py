2277ad422a97e3718ea779b9394080266cacddd8
Vikash Kumar <mailkumarvikash@gmail.com>
2016-03-14 02:21:17 +0530

    separate handler for blog api

---
+++ handler.py
@@ -0,0 +1,19 @@
+import logging
+
+import webapp2
+
+app = webapp2.WSGIApplication([
+        routes.RedirectRoute(
+            '/articles',
+            handler=blog.ArticleHandler, name='article', strict_slash=True),
+        routes.RedirectRoute(
+            '/subscribers',
+            handler=blog.SubscriberHandler,
+            name='subscriber', strict_slash=True),
+        routes.RedirectRoute(
+            '/tags',
+            handler=blog.TagHandler, name='tag', strict_slash=True),
+        routes.RedirectRoute(
+            '/auth',
+            handler=blog.AuthHandler, name='auth', strict_slash=True),
+    ])
