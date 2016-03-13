fa288c70de7dafe79cbce78443d55fa13c7bdcfb
Vikash Kumar <mailkumarvikash@gmail.com>
2016-03-13 22:14:37 +0530

    model schema for new blog

--- models/model.py
+++ models/model.py
@@ -1,21 +1,29 @@
 from google.appengine.ext import ndb
 
-
 class Article(ndb.Model):
     url = ndb.StringProperty()
     tittle = ndb.StringProperty()
-    date = ndb.DateTimeProperty()
+    date = ndb.DateTimeProperty()                        # has to be depricated
     content = ndb.TextProperty()
     shortUrl = ndb.StringProperty()
     stars = ndb.IntegerProperty(default=0)
     tags = ndb.StringProperty(repeated=True)
     published = ndb.BooleanProperty(default=True)
+    createdOn = ndb.DateTimeProperty()
+    modifiedOn = ndb.DateTimeProperty()
+    softDeleted = ndb.BooleanProperty(default=False)
 
 
 class Subscribers(object):
     name = ndb.StringProperty()
     email = ndb.StringProperty()
+    createdOn = ndb.DateTimeProperty()
+    modifiedOn = ndb.DateTimeProperty()
+    softDeleted = ndb.BooleanProperty(default=False)
 
 
 class Auth(ndb.Model):
     token = ndb.StringProperty()
+    createdOn = ndb.DateTimeProperty()
+    modifiedOn = ndb.DateTimeProperty()
+    softDeleted = ndb.BooleanProperty(default=False)
