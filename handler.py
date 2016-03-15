2695ac29d45898da020e283a7bd8ddcceabfca04
Vikash Kumar <mailkumarvikash@gmail.com>
2016-03-15 23:42:56 +0530

    handler bug fix:

--- handler.py
+++ handler.py
@@ -1,19 +1,22 @@
 import logging
 
 import webapp2
+from webapp2_extras import routes
+
+from controllers import blog
 
 app = webapp2.WSGIApplication([
         routes.RedirectRoute(
-            '/articlesgi',
+            '/api/articles',
             handler=blog.ArticleHandler, name='article', strict_slash=True),
         routes.RedirectRoute(
-            '/subscribers',
+            '/api/subscribers',
             handler=blog.SubscriberHandler,
             name='subscriber', strict_slash=True),
         routes.RedirectRoute(
-            '/tags',
+            '/api/tags',
             handler=blog.TagHandler, name='tag', strict_slash=True),
         routes.RedirectRoute(
             '/auth',
             handler=blog.AuthHandler, name='auth', strict_slash=True),
-    ])
+    ], debug=True)
