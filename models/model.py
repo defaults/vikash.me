1a13ab5ba8a46054fc6b5b1e00053f0d7d68a777
Vikash Kumar <mailkumarvikash@gmail.com>
2016-03-30 23:58:23 +0530

    added createdOn in models also added tags model

--- models/model.py
+++ models/model.py
@@ -3,31 +3,42 @@ from google.appengine.ext import ndb
 class Article(ndb.Model):
     url = ndb.StringProperty()
     tittle = ndb.StringProperty()
-    date = ndb.DateTimeProperty()                        # has to be depricated
+    date = ndb.DateTimeProperty()
     content = ndb.TextProperty()
     shortUrl = ndb.StringProperty()
     stars = ndb.IntegerProperty(default=0)
     tags = ndb.StringProperty(repeated=True)
     published = ndb.BooleanProperty(default=True)
-    createdOn = ndb.DateTimeProperty()
+    createdOn = ndb.DateTimeProperty(auto_now_add=True)
     modifiedOn = ndb.DateTimeProperty()
     softDeleted = ndb.BooleanProperty(default=False)
 
 
-class Subscribers(object):
+class Subscriber(object):
     name = ndb.StringProperty()
     email = ndb.StringProperty()
-    createdOn = ndb.DateTimeProperty()
+    createdOn = ndb.DateTimeProperty(auto_now_add=True)
     modifiedOn = ndb.DateTimeProperty()
     softDeleted = ndb.BooleanProperty(default=False)
 
 
 class Auth(ndb.Model):
     token = ndb.StringProperty()
-    createdOn = ndb.DateTimeProperty()
+    createdOn = ndb.DateTimeProperty(auto_now_add=True)
     modifiedOn = ndb.DateTimeProperty()
     softDeleted = ndb.BooleanProperty(default=False)
+
 
 class ShortUrl(ndb.Model):
     fullUrl = ndb.StringProperty()
     ShortUrl = ndb.StringProperty()
+    createdOn = ndb.DateTimeProperty(auto_now_add=True)
+    modifiedOn = ndb.DateTimeProperty()
+    softDeleted = ndb.BooleanProperty(default=False)
+
+
+class Tag(ndb.Model):
+    tag = ndb.StringProperty()
+    createdOn = ndb.DateTimeProperty(auto_now_add=True)
+    modifiedOn = ndb.DateTimeProperty()
+    softDeleted = ndb.BooleanProperty(default=False)
