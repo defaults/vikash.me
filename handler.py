bf5f1fa0869447b54e6b6794580ffe2968d0fee8
Vikash Kumar <mailkumarvikash@gmail.com>
2016-04-23 21:07:20 +0530

    handler modification

--- handler.py
+++ handler.py
@@ -15,21 +15,48 @@ app = webapp2.WSGIApplication([
         routes.RedirectRoute(
             '/api/articles',
             handler=blog.ArticleHandler,
-            name='article',
-            handler_method='all_articles', strict_slash=True, methods=['GET']),
+            name='get_all_articles',
+            handler_method='all_articles', methods=['GET'], strict_slash=True),
         routes.RedirectRoute(
             '/api/article',
-            handler=blog.ArticleHandler, name='article', strict_slash=True),
+            handler=blog.ArticleHandler, name='post_article',
+            methods=['POST'], strict_slash=True),
+        routes.RedirectRoute(
+            '/api/article/<id>',
+            handler=blog.ArticleHandler, name='article',
+            methods=['GET', 'PUT', 'DELETE'], strict_slash=True),
         routes.RedirectRoute(
             '/api/subscribers',
             handler=blog.SubscriberHandler,
-            name='subscriber', strict_slash=True),
+            name='get_all_subscribers', methods=['GET'], strict_slash=True),
+        routes.RedirectRoute(
+            '/api/subscriber',
+            handler=blog.SubscriberHandler,
+            name='post_subscriber', methods=['POST'], strict_slash=True),
+        routes.RedirectRoute(
+            '/api/subscriber/<id>',
+            handler=blog.SubscriberHandler,
+            name='subscriber', methods=['PUT', 'DELETE'], strict_slash=True),
+        routes.RedirectRoute(
+            '/api/tags',
+            handler=blog.TagHandler, name='get_all_tags',
+            methods=['GET'], strict_slash=True),
         routes.RedirectRoute(
             '/api/tag',
-            handler=blog.TagHandler, name='tag', strict_slash=True),
+            handler=blog.TagHandler, name='post_tag',
+            methods=['POST'], strict_slash=True),
+        routes.RedirectRoute(
+            '/api/tag/<id>',
+            handler=blog.TagHandler, name='delete_tag',
+            methods=['DELETE'], strict_slash=True),
         routes.RedirectRoute(
             '/api/short',
-            handler=blog.UrlShortnerHandler, name='short', strict_slash=True),
+            handler=blog.UrlShortnerHandler, name='short',
+            methods=['GET', 'PUT'], strict_slash=True),
+        routes.RedirectRoute(
+            '/api/short/<id>',
+            handler=blog.UrlShortnerHandler, name='delete_short',
+            methods=['DELETE'], strict_slash=True),
         routes.RedirectRoute(
             '/auth',
             handler=blog.BlogHandler,
