ffbba1dbd3a2d6b7d1320e99ba5048200355157d
Vikash Kumar <mailkumarvikash@gmail.com>
2017-03-26 04:07:30 +0530

    upgrading gulp and pakage file

--- gulpfile.js
+++ gulpfile.js
@@ -1,53 +1,120 @@
-// Include gulp
-var gulp = require('gulp');
+// Gulp.js configuration
+var
+    // Include gulp
+    gulp = require('gulp'),
 
-// Include Our Plugins
-var jshint = require('gulp-jshint');
-var sass = require('gulp-sass');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var rename = require('gulp-rename');
-var imagemin = require('gulp-imagemin');
-var cache = require('gulp-cache');
+    // Include Our Plugins
+    jshint = require('gulp-jshint'),
+    sass = require('gulp-sass'),
+    postcss = require('gulp-postcss'),
+    assets = require('postcss-assets'),
+    autoprefixer = require('autoprefixer'),
+    mqpacker = require('css-mqpacker'),
+    cssnano = require('cssnano'),
+    concat = require('gulp-concat'),
+    deporder = require('gulp-deporder'),
+    stripdebug = require('gulp-strip-debug'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    newer = require('gulp-newer'),
+    imagemin = require('gulp-imagemin'),
+    htmlclean = require('gulp-htmlclean'),
+    sourcemaps = require('gulp-sourcemaps'),
+
+    // development mode?
+    devBuild = (process.env.NODE_ENV !== 'production'),
+
+    // folders
+    folder = {
+        src: 'public/src/',
+        build: 'public/build/'
+    }
+;
 
 // Lint Task
 gulp.task('lint', function() {
-    return gulp.src('js/*.js')
+    return gulp.src('scripts/*.js')
         .pipe(jshint())
         .pipe(jshint.reporter('default'));
 });
 
-// Compile Our Sass
-gulp.task('sass', function() {
-    return gulp.src('scss/*.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('dist/css'));
-});
-
-// Concatenate & Minify JS
+// JavaScript processing
 gulp.task('scripts', function() {
-    return gulp.src('js/*.js')
-        .pipe(concat('all.js'))
-        .pipe(gulp.dest('dist'))
-        .pipe(rename('all.min.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('dist/js'));
+
+    var jsbuild = gulp.src(folder.src + 'scripts/**/*')
+        .pipe(deporder())
+        .pipe(concat('main.js'));
+
+    if (!devBuild) {
+        jsbuild = jsbuild
+            .pipe(stripdebug())
+            .pipe(uglify());
+    }
+
+    return jsbuild.pipe(gulp.dest(folder.build + 'scripts/'));
 });
 
 // compress and optimize images
 gulp.task('images', function() {
-    return gulp.src('src/images/**/*')
-    .pipe(cache(imagemin({ optimizationLevel: 5, progressive: true, interlaced: true })))
-    .pipe(gulp.dest('build/img'));
+    var out = folder.build + 'images/';
+    return gulp.src(folder.src + 'images/**/*')
+        .pipe(newer(out))
+        .pipe(imagemin(
+            {
+                optimizationLevel: 5,
+                progressive: true,
+                interlaced: true
+            }
+        ))
+        .pipe(gulp.dest(out));
+});
+
+// HTML processing
+gulp.task('html', ['images'], function() {
+    var
+        out = folder.build + 'templates/',
+        page = gulp.src(folder.src + 'templates/**/*')
+            .pipe(newer(out));
+
+    // minify production code
+    if (!devBuild) {
+        page = page.pipe(htmlclean());
+    }
+
+    return page.pipe(gulp.dest(out));
+});
+
+// CSS processing
+gulp.task('css', ['images'], function() {
+
+    var postCssOpts = [
+        assets({ loadPaths: ['images/'] }),
+        autoprefixer({ browsers: ['last 2 versions', '> 2%'] }),
+        mqpacker
+    ];
+
+    if (!devBuild) {
+        postCssOpts.push(cssnano);
+    }
+
+    return gulp.src(folder.src + 'stylesheets/main.scss')
+        .pipe(sass({
+            outputStyle: 'nested',
+            imagePath: 'images/',
+            precision: 3,
+            errLogToConsole: true
+        }))
+        .pipe(postcss(postCssOpts))
+        .pipe(gulp.dest(folder.build + 'stylesheets/'));
 });
 
 // Watch Files For Changes
 gulp.task('watch', function() {
     gulp.watch('js/*.js', ['lint', 'scripts']);
-    gulp.watch('scss/*.scss', ['sass']);
+    gulp.watch('scss/*.scss', ['css']);
     gulp.watch('src/images/**/*', ['images']);
 });
 
 
 // Default Task
-gulp.task('default', ['lint', 'sass', 'scripts', 'images', 'watch']);
+gulp.task('default', ['lint', 'css', 'scripts', 'images', 'html', 'watch']);
